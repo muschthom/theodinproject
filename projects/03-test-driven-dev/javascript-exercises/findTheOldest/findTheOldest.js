d8e48b07ff4bcfe3b256ee91c0ff9845290cc69e
Thomas <tom.travels.canada@gmail.com>
2019-07-28 18:09:09 +0200

    find the oldest erledigt

--- projects/03-test-driven-dev/javascript-exercises/findTheOldest/findTheOldest.js
+++ projects/03-test-driven-dev/javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
+let findTheOldest = function (arr) {
 
-}
+    //calculate age
+    arr.forEach(function (item) {
+
+        //check if still alive
+        if(item.yearOfDeath === undefined){
+            item.yearOfDeath = 2019;
+        }
+        item.age = item.yearOfDeath - item.yearOfBirth;
+    });
+
+    //sort arr
+    arr.sort(function (a, b) {
+        return a.age - b.age;
+    });
+
+    //return last item
+    return arr[arr.length-1];
+};
 
 module.exports = findTheOldest
