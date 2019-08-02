cad8f57055d74f72c6fd0b8a3c17b144910f987d
Thomas <tom.travels.canada@gmail.com>
2019-08-02 07:59:38 +0200

    Scrollen und URL Leiste ausblenden

--- projects/06-calculator/script.js
+++ projects/06-calculator/script.js
@@ -1,3 +1,12 @@
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
+
+window.onscroll = function () { window.scrollTo(0, 0); };
+
 let operate = function (a, b, calc) {
     switch (calc) {
         case "+":
