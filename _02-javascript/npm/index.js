471d55f7c492e9d386a7713e91b41ccebc92001d
Tom <tom.travels.canada@gmail.com>
2019-08-10 11:19:16 +0200

    erstes npm Projekt nach Anleitung erstellt

---
+++ _02-javascript/npm/index.js
@@ -0,0 +1,7 @@
+// index.js
+import moment from 'moment';console.log("Hello from JavaScript - vx!");
+console.log(moment().startOf('day').fromNow());var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
+console.log("Hello from JavaScript2!");
+console.log("another line");
+console.log("and the last line");
