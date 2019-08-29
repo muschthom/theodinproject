a1ba7046f1ffaa1fd726490f3940d3c7d7cbe7dc
Thomas <tom.travels.canada@gmail.com>
2019-08-29 17:42:12 +0200

    front end form validation exercise

---
+++ _02-javascript/frontend-form-validation/script.js
@@ -0,0 +1,49 @@
+let email = document.getElementById("email");
+
+email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("I expect an e-mail, darling!");
+    } else {
+        email.setCustomValidity("");
+    }
+});
+
+let emailConfirm = document.getElementById("email-confirm");
+
+emailConfirm.addEventListener("input", function (event) {
+    let email = document.getElementById("email").value;
+    let emailConformation = emailConfirm.value;
+    console.log("email = " + email);
+    console.log("email confirm = " + emailConformation);
+    if (email === emailConformation){
+        emailConfirm.setCustomValidity("");
+    } else {
+        emailConfirm.setCustomValidity("I expect the same e-mail, darling!");
+
+    }
+});
+
+let password = document.getElementById("password");
+
+password.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("I expect an 8 or more characters with at least one number, and one uppercase and lowercase character password, darling!");
+    } else {
+        email.setCustomValidity("");
+    }
+});
+
+let passwordConfirm = document.getElementById("password-confirm");
+
+passwordConfirm.addEventListener("input", function (event) {
+    let password = document.getElementById("password").value;
+    let passwordConformation = passwordConfirm.value;
+    console.log("password = " + password);
+    console.log("password confirm = " + passwordConformation);
+    if (password === passwordConformation){
+        passwordConfirm.setCustomValidity("");
+    } else {
+        passwordConfirm.setCustomValidity("I expect the same password, darling!");
+
+    }
+});
