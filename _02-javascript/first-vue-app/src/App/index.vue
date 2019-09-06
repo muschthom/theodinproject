28dd42c9388b43bc73969024432f5cfdbcfdbf1f
Thomas <tom.travels.canada@gmail.com>
2019-09-06 13:24:29 +0200

    first vue app

---
+++ _02-javascript/first-vue-app/src/App/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <p>Hello World</p>
+        <github-input></github-input>
+        <github-output></github-output>
+    </div>
+</template>
+<script src="./script.js"></script>
+<style scoped src="./style.css"></style>
---
+++ _02-javascript/first-vue-app/src/GithubOutput/script.js
@@ -0,0 +1,38 @@
+import bus from '../bus'
+import Vue from 'vue'
+import GithubUserData from '../GithubUserData/index.vue'
+
+export default {
+    name: 'GithubOutput',
+    components: {
+        'github-user-data': GithubUserData,
+    },
+    data() {
+        return {
+            currentUsername: null,
+            githubData: {}
+        }
+    },
+    created() {
+        bus.$on('new-username', this.onUsernameChange)
+    },
+    destroyed() {
+        bus.$off('new-username', this.onUsernameChange)
+    },
+    methods: {
+        onUsernameChange(name) {
+            this.currentUsername = name
+            this.fetchGithubData(name)
+        },
+        fetchGithubData(name) {
+            // if we have data already, don't request again
+            if (this.githubData.hasOwnProperty(name)) return
+
+            const url = `https://api.github.com/users/${name}`
+            fetch(url)
+                .then(r => r.json())
+                .then(data => {
+                    Vue.set(this.githubData, name, data)                })
+        }
+    },
+}
